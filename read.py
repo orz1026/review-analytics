206fdf7d43575403f76ecddbc7635f7a789e3bd3
alpha <orz1026@hotmail.com>
2021-01-15 09:27:27 +0800

    calc avg

--- read.py
+++ read.py
@@ -6,9 +6,22 @@ with open('reviews.txt', 'r') as f:
 		count += 1 
 		if count % 1000 == 0: # 求1000餘數
 		    print(len(data)) # 讀取進度
-print(len(data)) # 印出幾筆資料(長度)
+print('檔案讀取完畢， 總共有', len(data), '筆資料')
+# print(len(data)) # 印出幾筆資料(長度)
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d) #n算留言長度
+	#print(sum_len)
+
+print('平均是', sum_len / len(data)) # 留言平均長度
+
+
+
+
+
+
 
 # print(data) # 印出資料全部資料
-print(data[0]) # 印出特地資料
-print('------------------')
-print(data[1]) 
+# print(data[0]) # 印出特地資料
+# print('------------------')
+# print(data[1]) 
