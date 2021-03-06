2daf7203fddd2ff3278a48abfe29119f65d32b85
alpha <orz1026@hotmail.com>
2021-03-06 17:47:16 +0800

    add word count

--- read.py
+++ read.py
@@ -7,6 +7,13 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0: # 求1000餘數
 		    print(len(data)) # 讀取進度
 print('檔案讀取完畢， 總共有', len(data), '筆資料')
+
+print(data[0])
+
+
+
+
+
 # print(len(data)) # 印出幾筆資料(長度)
 sum_len = 0
 for d in data:
@@ -27,13 +34,53 @@ print('共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])   
 
+# 留言裡面有幾筆提到good
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('共有', len(good), '筆留言提到good')
+print(good[0])
+
+#文字計數
+wc = {} #word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 #新增新的key進wc字典
+for word in wc:
+	if wc[word] > 1000000:
+	    print(word, wc[word])
+
+print(len(wc))
+print(wc['alpha'])
+
+while True:
+	word = input('請問你要查甚麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+	    print(word, '出現過的次數: ', wc[word])
+	else:
+		print('這個字沒有出現過!')
 
 
+print('感謝使用查詢功能')
 
 
-
-
+# 清單快寫法 
+good = [d for d in data if 'good' in d]
+pront(god889)
 # print(data) # 印出資料全部資料
 # print(data[0]) # 印出特地資料
 # print('------------------')
 # print(data[1]) 
+
+
+
+
+
+
